1db95f3670bc6a3df7aa3da84b96fc79dadacda6
fgmcolas <https://github.com/fgmcolas>
2024-05-20 21:03:15 +0200

    new code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,15 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', function(data) {
-  const name = data.toString().trim();
+process.stdin.resume();
 
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-  process.stdout.write("This important software is now closing\n");
-  process.exit();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
